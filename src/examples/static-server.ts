06bc4f466fd25c3ebf628d64b15de7538b1ad083
Dima Doronin <doronindm@gmail.com>
2021-01-03 15:26:58 -0500

    push assets

--- src/examples/static-server.ts
+++ src/examples/static-server.ts
@@ -1,4 +1,4 @@
-import http2 from 'http2';
+import http2, { ServerHttp2Stream } from 'http2';
 import path from 'path';
 import fs from 'fs';
 import mime from 'mime-types';
@@ -12,14 +12,28 @@ const server = http2.createSecureServer({
 const staticPath = path.resolve(__dirname, './static');
 const staticPath404Page = path.resolve(__dirname, './static/404.html');
 
+function respond(fullPath: string, stream: ServerHttp2Stream) {
+    fs.createReadStream(fullPath).pipe(stream).respond({ 'content-type': mime.lookup(fullPath) as string });
+}
+
 const staticRouter = new RouterFactory();
 staticRouter.get("/:static*", (stream, headers) => {
     const fullPath = path.join(staticPath, headers[':path'] as string);
-    if (!fs.existsSync(fullPath)) {
-        return fs.createReadStream(staticPath404Page).pipe(stream).respond({'content-type': 'text/html'});
+    if (fs.existsSync(fullPath)) {
+        respond(fullPath, stream);
+    } else {
+        if (stream.pushAllowed) {
+            stream.pushStream({ ':path': '/img/404-error.jpg' }, (err, pushStream) => {
+                if (err) throw err;
+                respond(path.join(staticPath, './img/404-error.jpg'), pushStream);
+                pushStream.on('error', (err) => {
+                    const isRefusedStream = (err as any).code === 'ERR_HTTP2_STREAM_ERROR';
+                    if (!isRefusedStream) throw err;
+                  });
+            });
+        }
+        respond(staticPath404Page, stream);
     }
-
-    fs.createReadStream(fullPath).pipe(stream).respond({ 'content-type': mime.lookup(fullPath) as string });
 });
 
 useHttp2Server(server, [
